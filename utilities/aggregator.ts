670c85156afde447f9861b6c93ccdb228c35d576
Talha <talhaanwar.anwar@gmail.com>
2022-01-27 04:26:21 +0530

    Add functionality to sort date correctly

--- utilities/aggregator.ts
+++ utilities/aggregator.ts
@@ -1,3 +1,5 @@
+import moment from 'moment';
+
 function getTotalsForEntities(collection, sort=true) {
     
     const entitiesSumObj = collection.reduce(function(result, item) {
@@ -15,6 +17,17 @@ function getTotalsForEntities(collection, sort=true) {
   }
 
 
+const sortDateMoments = (testObj) => {
+    const orderedDates = {}
+    Object.keys(testObj).sort(function(a, b) {
+        return (moment(a, 'DD/MM/YYYY').toDate() as any) - (moment(b, 'DD/MM/YYYY').toDate() as any);
+    }).forEach(function(key) {
+        orderedDates[key] = testObj[key];
+    })
+    return orderedDates;
+}
+
+
   function getTotalsForDates(collection, sort=true) {
     
     const entitiesSumObj = collection.reduce(function(result, item) {
@@ -26,7 +39,7 @@ function getTotalsForEntities(collection, sort=true) {
     if(sort) {
         const sorted = {}
         Object.keys(entitiesSumObj).sort ((a,b) => entitiesSumObj[b] - entitiesSumObj[a]).map(item => sorted[item] = entitiesSumObj[item]);
-        result = sorted;
+        result = sortDateMoments(sorted);
     }
     return result;
   }
